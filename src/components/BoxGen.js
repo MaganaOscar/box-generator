0771cb04f6a89ece07590b2623e2dbc595efd069
MaganaOscar <oscmag13@gmail.com>
2021-07-07 15:18:39 -0700

    added color refresh on input and size input

--- src/components/BoxGen.js
+++ src/components/BoxGen.js
@@ -2,13 +2,23 @@ import React, {useState} from 'react';
 
 const BoxGen = (props) => {
     const[box, setBox] = useState("");
-    const [boxes, setBoxes] = useState([]);
+    const[size, setSize] = useState(2);
+    const [boxes] = useState([]);
 
     const createBox = (e) => {
         e.preventDefault();
+        console.log(e.target.color.value)
+        console.log(e.target.size.value)
+        if(e.target.size.value === ""){
+            e.target.size.value = 4;
+        }
         const newBox = e.target.color.value;
+        const newSize = e.target.size.value;
+        e.target.color.value = ""
+        e.target.size.value = 4;
         setBox(newBox);
-        boxes.push(newBox)
+        setSize(newSize);
+        boxes.push({color: newBox, size: newSize})
     };
 
     return (
@@ -18,12 +28,16 @@ const BoxGen = (props) => {
                     <label>Color </label>
                     <input name="color"/>
                 </div>
+                <div>
+                    <label>Size </label>
+                    <input type="number" min="4" name="size"/>
+                </div>
                 <button>Submit</button>
             </form>
             <div className="flex">
             {
                 boxes.map((box) => 
-                    <p className="box" style={{backgroundColor: box}}>{box}</p>
+                    <p className="box" style={{backgroundColor: box.color, width: box.size+"em", height: box.size+"em"}}>{box.color}</p>
                 )
             }
             </div>
